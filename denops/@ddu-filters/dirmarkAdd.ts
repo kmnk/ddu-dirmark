60cd40ea3a8c99b2ce86ec68e85fb2a127552309
KMNK <kmnknmk+com-github@gmail.com>
2022-11-04 23:23:25 +0900

    wip

---
+++ denops/@ddu-filters/dirmarkAdd.ts
@@ -0,0 +1,29 @@
+import {
+  BaseFilter,
+  DduItem,
+  SourceOptions,
+} from "https://deno.land/x/ddu_vim@v1.13.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v1.11.0/deps.ts";
+
+type Params = Record<never, never>;
+
+export class Filter extends BaseFilter<Params> {
+  async filter(args: {
+    denops: Denops;
+    sourceOptions: SourceOptions;
+    input: string;
+    items: DduItem[];
+  }): Promise<DduItem[]> {
+    for (const item of args.items) {
+      item.kind = "ddu-";
+      if (item.display) {
+        item.matcherKey = item.display;
+      }
+    }
+    return Promise.resolve(args.items);
+  }
+
+  params(): Params {
+    return {};
+  }
+}
